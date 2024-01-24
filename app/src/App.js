9eefe34f94d738421a0ed24e790126c5ab15851e
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2024-01-24 13:32:18 -0600

    begin base dashboard creation

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AP FRQ Grading App wsg
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <button className="DashboardButton">
+          Rubrics
+        </button>
       </header>
     </div>
   );
