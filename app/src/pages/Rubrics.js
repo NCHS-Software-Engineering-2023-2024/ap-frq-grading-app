06e9bfb94aba133a012194a3ffd7a053e6ff23be
Hyder Rizvi <hmrizvi@stu.naperville203.org>
2024-02-12 13:19:28 -0600

    Work on editable rows and columns

--- app/src/pages/Rubrics.js
+++ app/src/pages/Rubrics.js
@@ -1,11 +1,31 @@
 import RubricHeader from '../components/RubricHeader';
 import {useNavigate} from 'react-router-dom';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 
+function Table() {
+    useEffect(()=> {
+        axois.get('http://localhost:3000/rubrics')
+        .then(res => console.log(res))
+        .catch(er => console.log(er))
+    }, [])
+}
 
-  
+/*  
+function AddStandard(Rubric) {
+    var table = document.getElementById(Rubric);
+    var Standard = table.insertRow();
+    Standard.className  = "RubricInputField";
+    //Standard.colspan = 4;
+    var Numerals = table.insertRow();
+    var Description = table.insertRow();
+    var Standard1 = Standard.insertCell(0);
+    Standard1.colspan = 4;
+    
 
+    Standard1.innerHTML = "NEW CELL1";                                    
+}
+*/
 
 export default function Rubrics() {
     const navigate = useNavigate();
@@ -28,43 +48,51 @@ export default function Rubrics() {
             }}>
                 
             <input type="text" id="Title" name="RubricTitle" placeholder="Enter Title Here"  className = "RubricTitleInput"></input>
-
+            
             <button
                 type="button"
                 onClick={() => setTitle(document.getElementById('Title').value)}
                 className="ConfirmTitleButton"
             >Confirm</button>
 
+            
+
             </body>
             </>
             
             
-
-            <table className="RubricOutline">
-                            
-                <tr>
-                    <th colspan = "4">
-                        <input type="text" id="Standard" name="RubricStandard" placeholder="Enter Standard Here" className = "RubricInputField"></input>
-                    </th>
-                </tr>
-                
-                <tr>
-                    <th> 1 </th>
-                    <th> 2 </th>
-                    <th> 3 </th>
-                    <th> 4 </th>
-                </tr>
-
-                <tr>
-                    <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
-                    <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
-                    <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
-                    <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
-                </tr>
-
-                
-            </table>
             
+                <table className="RubricOutline" id="Rubric">
+                                
+                    <tr>
+                        <th colspan = "4">
+                            <input type="text" id="Standard" name="RubricStandard" placeholder="Enter Standard Here" className = "RubricInputField"></input>
+                        </th>
+                    </tr>
+                    
+                    <tr>
+                        <th> 1 </th>
+                        <th> 2 </th>
+                        <th> 3 </th>
+                        <th> 4 </th>
+                    </tr>
+
+                    <tr>
+                        <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
+                        <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
+                        <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
+                        <td> <input type="text" id="Description1" name="Description1" placeholder="Enter Description Here" className = "RubricInputField"></input> </td>
+                    </tr>
+
+                    
+                </table>
+            
+                <button
+                    type="button"
+                    onClick={() => AddStandard("Rubric")}
+                    className="InsertRowButton"
+                >Add Row</button>
+
         </div>
 
 
