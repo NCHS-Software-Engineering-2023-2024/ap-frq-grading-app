dcf78f08bbfa9b1326a091b43e799d984d01476d
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2024-02-28 13:36:23 -0600

    attempt server setup

--- app/server/server.js
+++ app/server/server.js
@@ -1,9 +1,24 @@
 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/message', (req, res) => {
+    res.json({ message: "AP FRQ Database" });
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+  });
 
 
-var mysql = require('mysql');
-var con = mysql.createConnection({
-host: "localhost:3306",
-user: "root",
-password: "AP_FRQ$2024!",
-database: "APFRQ_Database"});
+
+// var mysql = require('mysql');
+// var con = mysql.createConnection({
+// host: "localhost:3306",
+// user: "root",
+// password: "AP_FRQ$2024!",
+// database: "APFRQ_Database"});
