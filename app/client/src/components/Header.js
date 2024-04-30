df109a0001e0b50c3598d6c129e00b85efe1b896
Stefan Ilic <SIlic@stu.naperville203.org>
2024-04-30 13:16:58 -0500

    update css to look cool

--- app/client/src/components/Header.js
+++ app/client/src/components/Header.js
@@ -39,34 +39,34 @@ export default function Header() {
     };
     return (
         <div className="App">
-            <header className="App-header">
-            <GoogleOAuthProvider clientId="78244490523-lobbagoj7mhobmmc9c8uv06164ivbtqr.apps.googleusercontent.com">
-                <br />
-                <br />
-                {profile ? (
-                    <div>
-                        <img src={profile.picture} alt="user image" />
-                        <h3>User Logged in</h3>
-                        <p>Name: {profile.name}</p>
-                        <p>Email Address: {profile.email}</p>
-                        <br />
-                        <br />
-                        <button onClick={logOut}>Log out</button>
+            <header className="App-header" style={{ position: 'fixed', top: 10, left: 10 }}>
+                <GoogleOAuthProvider clientId="78244490523-lobbagoj7mhobmmc9c8uv06164ivbtqr.apps.googleusercontent.com">
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        {profile ? (
+                            <div>
+                                <img src={profile.picture} alt="user image" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />
+                                <div style={{ marginLeft: '10px' }}>
+                                    <h3>User Logged in</h3>
+                                    <p>Name: {profile.name}</p>
+                                    <p>Email Address: {profile.email}</p>
+                                </div>
+                                <button onClick={logOut} style={{ marginLeft: '10px' }}>Log out</button>
+                            </div>
+                        ) : (
+                            <button onClick={login} style={{ fontSize: '20px' }}>Sign in with Google 🚀 </button>
+                        )}
                     </div>
-                ) : (
-                    <button onClick={login}>Sign in with Google 🚀 </button>
-                )}
                 </GoogleOAuthProvider>
 
                 <p>
-                Dashboard
+                    Dashboard
                 </p>
 
-                <button className="InfoTab" onClick={()=>navigate("/info")}>
+                <button className="InfoTab" onClick={() => navigate("/info")}>
                     Info
                 </button>
 
-                
+
             </header>
         </div>
     )
