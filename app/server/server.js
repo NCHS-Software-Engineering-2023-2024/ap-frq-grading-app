ed35b3d104882cab723582bf270f083f1e6d7ec3
Hyder Rizvi <111460999+hyderrizvi06@users.noreply.github.com>
2024-05-01 13:33:26 -0500

    Manually merged the rubrics branch with the main branch

--- app/server/server.js
+++ app/server/server.js
@@ -19,6 +19,36 @@ var mysql = require('mysql');
   database: "redhawks_rubric"
 });
 
+app.get('/rubricsInfo', (req, res) => {
+  const sql = "SELECT rubric.rubricName AS rubricTitle, rubric.RubricID AS rubricNum, standard.standardName as standard, cell.idStandard AS standardNum, cell.cellNum, cell.cellDesc FROM cell INNER JOIN standard ON cell.idStandard = standard.idStandard JOIN rubric ON standard.idRubric = rubric.RubricID;";
+  con.query(sql, (err, data) => {
+    console.log(data);
+    if (err) return res.json(err);
+    return res.json(data);
+  })
+});
+
+app.post('/rubricsInfo', (req, res) => {
+    let statement = "";
+    const {desc, id, path} = req.body;
+    if(path == 1){
+      statement = "UPDATE `cell` SET `cellDesc` = (?) WHERE `cell`.`idCell` = (?);";
+    }
+    else if(path == 2){
+      statement = "UPDATE `standard` SET `standardName` = (?) WHERE `standard`.`idStandard` = (?);";
+    }
+    else{
+      statement = "UPDATE `rubric` SET `rubricName` = (?) WHERE `rubric`.`rubricID` = (?);";
+    }
+    
+    con.query(statement, [desc, id], (err, data) => {
+      console.log("Complete!" +desc+" "+id);
+      if (err) return res.json(err);
+      return console.log(data);
+
+    })
+    
+});
 
 app.get('/rubrics', (req, res) => {
   const sql = "SELECT * FROM Rubric";
