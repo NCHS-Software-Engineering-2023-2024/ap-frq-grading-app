8197a4b512e5088ecf313acf3893cab7aa9f6c2a
ksrinivasan11 <111408308+ksrinivasan11@users.noreply.github.com>
2024-05-16 12:36:14 -0500

    meta tags

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -25,8 +25,28 @@ app.listen(PORT, (error) => {
       console.log("listening on " + PORT + "...");
   });
 app.get('/*', (req, res, next) => {
-    // TODO
-  });
+    fs.readFile(indexPath, 'utf8', (err, htmlData) => {
+        if (err) {
+            console.error('Error during file reading', err);
+            return res.status(404).end()
+        }
+        // get post info
+        const postId = req.params.id;
+        const post = getPostById(postId);
+        if(!post) return res.status(404).send("Post not found");
+
+        // inject meta tags
+        htmlData = htmlData.replace(
+            "<title>React App</title>",
+            `<title>${post.title}</title>`
+        )
+        .replace('__META_OG_TITLE__', post.title)
+        .replace('__META_OG_DESCRIPTION__', post.description)
+        .replace('__META_DESCRIPTION__', post.description)
+        .replace('__META_OG_IMAGE__', post.thumbnail)
+        return res.send(htmlData);
+    });
+});
 
 root.render(
   <GoogleOAuthProvider clientId="78244490523-lobbagoj7mhobmmc9c8uv06164ivbtqr.apps.googleusercontent.com">
